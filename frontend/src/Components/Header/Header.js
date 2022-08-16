48ede5feba5eee5b690ac0da2cdfc42eaffec905
Raydertrent <50339548+Raydertrent@users.noreply.github.com>
2022-08-16 11:59:52 -0600

    hotfixes
    -hotfixes on Header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Container,
   Form,
@@ -10,45 +10,67 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Header = (username) => {
+var userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
-  return (
-    <Navbar
-      collapseOnSelect
-      key={"lg"}
-      bg="primary"
-      expand="lg"
-      className="mb-3"
-    >
-      <Container>
-        <Navbar.Brand href="/">NoteDev</Navbar.Brand>
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        {'Javi mae aqui va la funcion para ver que usuario esta conectado o logeado ya con eso se muesta cuando se debe'}
-        { username==!" "?(
-          <div>
+    this.state = {
+      userInfo: JSON.parse(localStorage.getItem("userInfo")),
+      logged: true,
+    };
+  }
+
+  logout() {
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("noteInfo");
+    window.location.replace("/");
+  }
+
+  render() {
+    return (
+      <Navbar
+        collapseOnSelect
+        key={"lg"}
+        bg="primary"
+        expand="lg"
+        className="mb-3"
+      >
+        <Container>
+          <Navbar.Brand href="/">NoteDev</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          {this.state.userInfo ? (
+            <div>
               <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <Nav.Link href="notes">
-                  My notes
-                  <Link to="/notes" />
-                </Nav.Link>
-
-                <Nav.Link href="/">
-                  Log out
-                  <Link to="/" />
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        ):(<></>)}
-        
-      </Container>
-    </Navbar>
-  );
-};
-export default Header;
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Button
+                    onClick={() => {
+                      window.location.replace("/notes");
+                    }}
+                  >
+                    My notes
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      this.logout();
+                    }}
+                  >
+                    Log out
+                  </Button>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          ) : (
+            <></>
+          )}
+        </Container>
+      </Navbar>
+    );
+  }
+}
