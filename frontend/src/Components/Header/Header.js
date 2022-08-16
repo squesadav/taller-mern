5e550b2636d290e1930a6787a7d2f317225b58f5
Justin Bogantes <justin.bogantes99@gmail.com>
2022-08-16 02:26:14 -0600

    .

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -10,10 +10,8 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  //this.state = {
-  //username: 'Justin Bogantes Rodriguez'
-  //}
+const Header = (username) => {
+
   return (
     <Navbar
       collapseOnSelect
@@ -26,24 +24,29 @@ const Header = () => {
         <Navbar.Brand href="/">NoteDev</Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbarScroll" />
+        {'Javi mae aqui va la funcion para ver que usuario esta conectado o logeado ya con eso se muesta cuando se debe'}
+        { username==!" "?(
+          <div>
+              <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="notes">
+                  My notes
+                  <Link to="/notes" />
+                </Nav.Link>
 
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="notes">
-              My notes
-              <Link to="/notes" />
-            </Nav.Link>
-
-            <Nav.Link href="/">
-              Log out
-              <Link to="/" />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+                <Nav.Link href="/">
+                  Log out
+                  <Link to="/" />
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        ):(<></>)}
+        
       </Container>
     </Navbar>
   );
